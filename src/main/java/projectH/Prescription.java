810bbbedc0a2e5f387c16ec80f10cdd5db5fd895
Alexandre Gariépy <agariepy@the-machine.site>
2014-01-24 15:58:11 -0500

    Correction pom.xml. Les attributs n'étaient pas dans project

--- src/main/java/projectH/Prescription.java
+++ src/main/java/projectH/Prescription.java
@@ -1,5 +1,16 @@
 package projectH;
 
+import java.util.GregorianCalendar;
+
 public class Prescription {
+	private int practitioner;
+
+	public Prescription(int i, GregorianCalendar date, int j, String string) {
+		this.practitioner = i;
+	}
+
+	public int getPractioner() {
+		return practitioner;
+	}
 
 }
--- src/test/java/projectH/PrescriptionTest.java
+++ src/test/java/projectH/PrescriptionTest.java
@@ -1,5 +1,18 @@
 package projectH;
 
+import static org.junit.Assert.*;
+
+import java.util.GregorianCalendar;
+
+import org.junit.Test;
+
 public class PrescriptionTest {
 
+	@Test
+	public void canGetPractionerNumber() {
+		GregorianCalendar date = new GregorianCalendar(1970, 1, 1, 12, 0);
+		Prescription aPrescription = new Prescription(123123, date, 0, "ADVIL");
+		assertEquals(123123, aPrescription.getPractioner());
+	}
+	
 }
