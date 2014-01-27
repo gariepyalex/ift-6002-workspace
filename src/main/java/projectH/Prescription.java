ba11188d003f7ccfaa22d31982266b290d4616fb
moduv5 <marc-olivier.duval.1@ulaval.ca>
2014-01-27 15:14:27 -0500

    Refactor the getDate
    Refactor the getDate method in prescription class. + correct the test
    related

--- src/main/java/projectH/Prescription.java
+++ src/main/java/projectH/Prescription.java
@@ -1,16 +1,19 @@
 package projectH;
 
-import java.util.Calendar;
-import java.util.GregorianCalendar;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Prescription {
 	private int practitioner;
-	private GregorianCalendar date;
+	private Date date;
 	private int renewals;
+	private static final String DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
 
-	public Prescription(int practitioner, GregorianCalendar date, int renewals, String medecineName) throws InvalidPrescriptionException {
-		if(renewals < 0)
-			throw new InvalidPrescriptionException("The number of renewals must be greater or equals than zero");
+	public Prescription(int practitioner, Date date, int renewals,
+			String medecineName) throws InvalidPrescriptionException {
+		if (renewals < 0)
+			throw new InvalidPrescriptionException(
+					"The number of renewals must be greater or equals than zero");
 		this.practitioner = practitioner;
 		this.date = date;
 		this.renewals = renewals;
@@ -21,10 +24,8 @@ public class Prescription {
 	}
 
 	public String getDate() {
-		String formatedDate = String.format("%d-%02d-%02dT%02d:%02d:%02d",
-				date.get(Calendar.YEAR), date.get(Calendar.MONTH), date.get(Calendar.DATE),
-				date.get(Calendar.HOUR_OF_DAY), date.get(Calendar.MINUTE), date.get(Calendar.SECOND));
-		return formatedDate;
+		SimpleDateFormat df = new SimpleDateFormat(DATE_FORMAT);
+		return df.format(date);
 	}
 
 	public int getRenewals() {
--- src/test/java/projectH/PrescriptionTest.java
+++ src/test/java/projectH/PrescriptionTest.java
@@ -1,57 +1,59 @@
 package projectH;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+import java.util.Date;
 import java.util.GregorianCalendar;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class PrescriptionTest {
-	
 
 	private Prescription aDimPrescription;
-	
+
 	private final int A_PRACTITIONER_NUMBER = 123123;
-	
-	private final String A_FORMATTED_DATE = "1970-07-01T12:00:00";
+
+	private final String A_FORMATTED_DATE = "1970-07-01 12:00:00";
 	private final int A_YEAR = 1970;
-	private final int A_MONTH = 7;
+	private final int A_MONTH = 6;
 	private final int A_DAY = 1;
 	private final int A_HOUR = 12;
 	private final int A_MINUTE = 0;
 	private final int A_SECOND = 0;
-	
+
 	private final int A_VALID_RENEWALS = 0;
 	private final int AN_INVALID_RENEWALS = -1;
-	
+
 	@Before
 	public void givenADimPrescription() {
-		GregorianCalendar date = new GregorianCalendar(A_YEAR, A_MONTH, A_DAY,
-				A_HOUR, A_MINUTE, A_SECOND);
+		Date date = new GregorianCalendar(A_YEAR, A_MONTH, A_DAY, A_HOUR,
+				A_MINUTE, A_SECOND).getTime();
 		aDimPrescription = new Prescription(A_PRACTITIONER_NUMBER, date,
 				A_VALID_RENEWALS, "ADVIL");
 	}
+
 	@Test
 	public void canGetPractionerNumber() {
 		assertEquals(A_PRACTITIONER_NUMBER, aDimPrescription.getPractioner());
 	}
-	
+
 	@Test
 	public void canGetDate() {
+		System.out.print(aDimPrescription.getDate());
 		assertEquals(A_FORMATTED_DATE, aDimPrescription.getDate());
 	}
-	
+
 	@Test
 	public void canGetRenewals() {
 		assertEquals(A_VALID_RENEWALS, aDimPrescription.getRenewals());
 	}
-	
-	@Test(expected=InvalidPrescriptionException.class)
+
+	@Test(expected = InvalidPrescriptionException.class)
 	public void whenInvalidRenewalsIsInteredExceptionIsThrown() {
-		GregorianCalendar date = new GregorianCalendar(A_YEAR, A_MONTH, A_DAY,
-				A_HOUR, A_MINUTE, A_SECOND);
-		Prescription invalidPrescription = new Prescription(A_PRACTITIONER_NUMBER, date,
-				AN_INVALID_RENEWALS, "ADVIL");
+		Date date = new GregorianCalendar(A_YEAR, A_MONTH, A_DAY, A_HOUR,
+				A_MINUTE, A_SECOND).getTime();
+		Prescription invalidPrescription = new Prescription(
+				A_PRACTITIONER_NUMBER, date, AN_INVALID_RENEWALS, "ADVIL");
 	}
 }
