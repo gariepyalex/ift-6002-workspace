0291257ac6c8ac5212703572ac1914dcfe617d95
moduv5 <marc-olivier.duval.1@ulaval.ca>
2014-01-27 14:07:43 -0500

    Remove a warning in InvalidPrescriptionException

--- src/main/java/projectH/InvalidPrescriptionException.java
+++ src/main/java/projectH/InvalidPrescriptionException.java
@@ -1,6 +1,8 @@
 package projectH;
 
-public class InvalidPrescriptionException extends RuntimeException{
+public class InvalidPrescriptionException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
 
 	public InvalidPrescriptionException(String string) {
 		super(string);
