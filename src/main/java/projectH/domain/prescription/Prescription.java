32bd5f534b8b83718e1a0cfd020ad7864a28af2f
moduv5 <marc-olivier.duval.1@ulaval.ca>
2014-01-31 01:26:33 -0500

    Added some test case to match criteria

--- src/main/java/projectH/domain/prescription/Prescription.java
+++ src/main/java/projectH/domain/prescription/Prescription.java
@@ -7,15 +7,25 @@ public class Prescription {
 	private int practitioner;
 	private Calendar date;
 	private int renewals;
+	private String din;
+	private String medecineName;
+
 	private static final String DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss";
 
-	public Prescription(int practitioner, Calendar date, int renewals, String medecineName)
+	public Prescription(int practitioner, Calendar date, int renewals, String din, String medecineName)
 			throws InvalidPrescriptionException {
 		if (renewals < 0)
 			throw new InvalidPrescriptionException("The number of renewals must be greater or equals than zero");
+		if (!din.isEmpty() && !medecineName.isEmpty())
+			throw new InvalidPrescriptionException("You cannot set din and medecine name at the same time");
+		if (din.isEmpty() && medecineName.isEmpty())
+			throw new InvalidPrescriptionException("A din or medecine name must be set");
+
 		this.practitioner = practitioner;
 		this.date = date;
 		this.renewals = renewals;
+		this.din = din;
+		this.medecineName = medecineName;
 	}
 
 	public int getPractioner() {
@@ -29,6 +39,14 @@ public class Prescription {
 
 	public int getRenewals() {
 		return renewals;
+	}
+
+	public String getDin() {
+		return din;
+	}
+
+	public String getMedecineName() {
+		return medecineName;
 	}
 
 }
--- src/test/java/projectH/domain/prescription/PrescriptionTest.java
+++ src/test/java/projectH/domain/prescription/PrescriptionTest.java
@@ -23,11 +23,13 @@ public class PrescriptionTest {
 
 	private final int A_VALID_RENEWALS = 0;
 	private final int AN_INVALID_RENEWALS = -1;
+	private final String A_VALID_DIN = "111111";
+	private final String A_VALID_MEDECINE_NAME = "Advil";
 
 	@Before
 	public void givenADimPrescription() {
 		GregorianCalendar date = new GregorianCalendar(A_YEAR, A_MONTH, A_DAY, A_HOUR, A_MINUTE, A_SECOND);
-		aDimPrescription = new Prescription(A_PRACTITIONER_NUMBER, date, A_VALID_RENEWALS, "ADVIL");
+		aDimPrescription = new Prescription(A_PRACTITIONER_NUMBER, date, A_VALID_RENEWALS, A_VALID_DIN, "");
 	}
 
 	@Test
@@ -45,9 +47,33 @@ public class PrescriptionTest {
 		assertEquals(A_VALID_RENEWALS, aDimPrescription.getRenewals());
 	}
 
+	@Test
+	public void canGetDin() {
+		assertEquals(A_VALID_DIN, aDimPrescription.getDin());
+	}
+
+	@Test
+	public void canGetMedecineName() {
+		Prescription prescription = new Prescription(A_PRACTITIONER_NUMBER, new GregorianCalendar(), A_VALID_RENEWALS,
+				"", A_VALID_MEDECINE_NAME);
+		assertEquals(A_VALID_MEDECINE_NAME, prescription.getMedecineName());
+	}
+
 	@Test(expected = InvalidPrescriptionException.class)
 	public void whenInvalidRenewalsIsInteredExceptionIsThrown() {
 		GregorianCalendar date = new GregorianCalendar(A_YEAR, A_MONTH, A_DAY, A_HOUR, A_MINUTE, A_SECOND);
-		new Prescription(A_PRACTITIONER_NUMBER, date, AN_INVALID_RENEWALS, "ADVIL");
+		new Prescription(A_PRACTITIONER_NUMBER, date, AN_INVALID_RENEWALS, A_VALID_DIN, "");
+	}
+
+	@Test(expected = InvalidPrescriptionException.class)
+	public void whenDinAndMedecineNameIsInteredExceptionIsThrown() {
+		GregorianCalendar date = new GregorianCalendar(A_YEAR, A_MONTH, A_DAY, A_HOUR, A_MINUTE, A_SECOND);
+		new Prescription(A_PRACTITIONER_NUMBER, date, A_VALID_RENEWALS, A_VALID_DIN, A_VALID_MEDECINE_NAME);
+	}
+
+	@Test(expected = InvalidPrescriptionException.class)
+	public void whenNoDinOrMedecineNameIsInteredExceptionIsThrown() {
+		GregorianCalendar date = new GregorianCalendar(A_YEAR, A_MONTH, A_DAY, A_HOUR, A_MINUTE, A_SECOND);
+		new Prescription(A_PRACTITIONER_NUMBER, date, A_VALID_RENEWALS, "", "");
 	}
 }
