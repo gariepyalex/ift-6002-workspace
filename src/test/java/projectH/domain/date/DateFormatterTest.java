409b9c3f0b61df2f19c5e9a29f6d05f20a7ac95a
ALWAL12 <alexandre.walsh.1@ulaval.ca>
2014-02-11 18:09:10 -0500

    Refactor DateFormatterTest

--- src/test/java/projectH/domain/date/DateFormatterTest.java
+++ src/test/java/projectH/domain/date/DateFormatterTest.java
@@ -1,32 +1,49 @@
 package projectH.domain.date;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class DateFormatterTest {
 
-	private final String INVALID_DATE = "2002-12-1";
-	private final String VALID_DATE = "2002-12-12T12:01:00";
+	private final static String INVALID_DATE_FORMAT = "2002-12-1";
+	private final static String VALID_DATE_FORMAT = "2014-01-01T12:00:00";
+
+	private static Date VALID_DATE = new GregorianCalendar(2014, Calendar.JANUARY, 01, 12, 00, 00).getTime();
+
 	private DateFormatter dateFormatter;
 
 	@Before
-	public void givenAFactoryTest() {
+	public void setup() {
 		dateFormatter = new DateFormatter();
 	}
 
 	@Test(expected = DateException.class)
-	public void throwsDateExceptionIfDateIsInvalid() {
-		dateFormatter.parse(INVALID_DATE);
+	public void givenFormatterWhenParsingInvalidDateShouldThrowException() {
+		dateFormatter.parse(INVALID_DATE_FORMAT);
 	}
 
 	@Test
-	public void methodGetStringShouldReturnDateInStringWithRightFormat() {
-		Date date = dateFormatter.parse(VALID_DATE);
-		String dateStringReturned = dateFormatter.dateToString(date);
-		assertEquals(VALID_DATE, dateStringReturned);
+	public void givenFormatterWhenParsingValidDateShouldReturnDate() {
+		Date parsedDate = dateFormatter.parse(VALID_DATE_FORMAT);
+
+		assertEquals(VALID_DATE, parsedDate);
+	}
+
+	@Test
+	public void givenFormatterWhenConvertToStringShouldReturnDateWithSameFormat() {
+		String dateConverted = dateFormatter.dateToString(VALID_DATE);
+
+		assertEquals(VALID_DATE_FORMAT, dateConverted);
+	}
+
+	@Test(expected = DateException.class)
+	public void givenFormatterWhenConvertToStringWithInvalidFormatShouldReturnException() {
+		dateFormatter.parse(INVALID_DATE_FORMAT);
 	}
 }
