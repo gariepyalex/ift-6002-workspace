54f26a412f4fdadb3c593ed023bbde22360d3afd
Yohan Caron <yohan.caron1@gmail.com>
2014-02-11 20:21:09 -0500

    oublier deux fichiers

---
+++ src/main/java/projectH/application/assemblers/OperationAssembler.java
@@ -0,0 +1,9 @@
+package projectH.application.assemblers;
+
+import projectH.application.responses.OperationDTO;
+
+public class OperationAssembler {
+    public void toOperation(OperationDTO dto) {
+
+    }
+}
