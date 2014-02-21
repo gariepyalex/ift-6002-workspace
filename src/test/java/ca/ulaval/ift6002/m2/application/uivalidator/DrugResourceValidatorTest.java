3c2bfb51d42b40f647f35be77c7ad47f121e9b35
moduv5 <marc-olivier.duval.1@ulaval.ca>
2014-02-21 00:00:21 -0500

    !00% coverage in DrugResourceValidator

--- src/test/java/ca/ulaval/ift6002/m2/application/uivalidator/DrugResourceValidatorTest.java
+++ src/test/java/ca/ulaval/ift6002/m2/application/uivalidator/DrugResourceValidatorTest.java
@@ -5,13 +5,19 @@ import org.junit.Test;
 
 public class DrugResourceValidatorTest {
 
-    private static final String LESS_THAN_THREE_CHARACTERS_KEYWORD = "ab";
+    private static final String VALID_KEYWORD = "advil";
+    private static final String LESS_THAN_THREE_CHARACTERS_KEYWORD = "ad";
 
     private DrugResourceValidator drugResourceValidator;
 
     @Before
     public void setup() {
         drugResourceValidator = new DrugResourceValidator();
+    }
+
+    @Test
+    public void validateKeywordWhenValidKeywordShouldDoNothing() {
+        drugResourceValidator.validateKeyword(VALID_KEYWORD);
     }
 
     @Test(expected = IllegalArgumentException.class)
