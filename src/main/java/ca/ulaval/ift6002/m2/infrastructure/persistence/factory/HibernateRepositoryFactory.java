e82c3ac1c07d95d32e711cb380ac91bd4675bb13
moduv5 <marc-olivier.duval.1@ulaval.ca>
2014-02-23 02:01:24 -0500

    Added HibernateDrugRepository to HibernateRepositoryFactory

--- src/main/java/ca/ulaval/ift6002/m2/infrastructure/persistence/factory/HibernateRepositoryFactory.java
+++ src/main/java/ca/ulaval/ift6002/m2/infrastructure/persistence/factory/HibernateRepositoryFactory.java
@@ -4,12 +4,17 @@ import ca.ulaval.ift6002.m2.domain.drug.DrugRepository;
 import ca.ulaval.ift6002.m2.domain.instrument.InstrumentRepository;
 import ca.ulaval.ift6002.m2.domain.operation.OperationRepository;
 import ca.ulaval.ift6002.m2.domain.patient.PatientRepository;
+import ca.ulaval.ift6002.m2.infrastructure.persistence.assemblers.DrugDTOAssembler;
+import ca.ulaval.ift6002.m2.infrastructure.persistence.hibernate.HibernateDrugRepository;
+import ca.ulaval.ift6002.m2.infrastructure.persistence.hibernate.provider.EntityManagerProvider;
 
 public class HibernateRepositoryFactory implements RepositoryFactory {
 
     @Override
     public DrugRepository createDrugRepository() {
-        return null;
+        EntityManagerProvider entityManager = new EntityManagerProvider();
+        DrugDTOAssembler assembler = new DrugDTOAssembler();
+        return new HibernateDrugRepository(entityManager.getEntityManager(), assembler);
     }
 
     @Override
---
+++ src/test/java/ca/ulaval/ift6002/m2/infrastructure/persistence/factory/HibernateRepositoryFactoryTest.java
@@ -0,0 +1,25 @@
+package ca.ulaval.ift6002.m2.infrastructure.persistence.factory;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import ca.ulaval.ift6002.m2.domain.drug.DrugRepository;
+import ca.ulaval.ift6002.m2.infrastructure.persistence.hibernate.HibernateDrugRepository;
+
+public class HibernateRepositoryFactoryTest {
+
+    private RepositoryFactory repositoryFactory;
+
+    @Before
+    public void setup() {
+        repositoryFactory = new HibernateRepositoryFactory();
+    }
+
+    @Test
+    public void givenFactoryWhenCreateDrugRepositoryShouldReturnHibernateClass() {
+        DrugRepository drugRepository = repositoryFactory.createDrugRepository();
+        assertEquals(drugRepository.getClass(), HibernateDrugRepository.class);
+    }
+}
