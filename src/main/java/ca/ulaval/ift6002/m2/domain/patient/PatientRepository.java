fc1795d0ef4ce9799c38af2a9b7a760f20507eee
ALWAL12 <alexandre.walsh.1@ulaval.ca>
2014-02-23 00:08:53 -0500

    Minor refactor to PatientRepository
    Moved some logic from the repository to the service.

--- src/main/java/ca/ulaval/ift6002/m2/domain/patient/PatientRepository.java
+++ src/main/java/ca/ulaval/ift6002/m2/domain/patient/PatientRepository.java
@@ -1,15 +1,9 @@
 package ca.ulaval.ift6002.m2.domain.patient;
 
-import ca.ulaval.ift6002.m2.domain.prescription.Prescription;
 
 public interface PatientRepository {
 
-    /**
-     * TODO You should not have a method savePrescription, you should get your
-     * patient, append (update) your prescriptions and store it back to the
-     * repository
-     */
-    void savePrescription(Patient patient, Prescription prescription);
-
     Patient get(int id);
+
+    void store(Patient patient);
 }
