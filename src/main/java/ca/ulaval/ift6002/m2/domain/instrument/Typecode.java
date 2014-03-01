b293196ca4fff3f6f645244530b8569316017ac0
Dario Silva <dario@linux-3g8s.site>
2014-03-01 13:23:48 -0500

    equals builder typecode instrument

--- src/main/java/ca/ulaval/ift6002/m2/domain/instrument/Typecode.java
+++ src/main/java/ca/ulaval/ift6002/m2/domain/instrument/Typecode.java
@@ -2,32 +2,29 @@ package ca.ulaval.ift6002.m2.domain.instrument;
 
 import java.util.Objects;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+
 public class Typecode {
 
-    private final String value;
+	private final String value;
 
-    public Typecode(String value) {
-        this.value = value;
-    }
+	public Typecode(String value) {
+		this.value = value;
+	}
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(value);
-    }
+	@Override
+	public int hashCode() {
+		return Objects.hash(value);
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
+	@Override
+	public boolean equals(Object obj) {
 
-        if (!(obj instanceof Typecode)) {
-            return false;
-        }
-
-        Typecode other = (Typecode) obj;
-
-        return Objects.equals(value, other.value);
-    }
-
+		if ((obj instanceof Typecode)) {
+			Typecode other = (Typecode) obj;
+			return new EqualsBuilder().append(value, other.value).isEquals();
+		} else {
+			return false;
+		}
+	}
 }
