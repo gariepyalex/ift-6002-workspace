e8c702d765b9dc2aa834df582955b5c4e20fced1
Dario Silva <dario@linux-3g8s.site>
2014-03-01 13:13:53 -0500

    equals builder seria number of instrument

--- src/main/java/ca/ulaval/ift6002/m2/domain/instrument/Serial.java
+++ src/main/java/ca/ulaval/ift6002/m2/domain/instrument/Serial.java
@@ -2,36 +2,35 @@ package ca.ulaval.ift6002.m2.domain.instrument;
 
 import java.util.Objects;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+
 public class Serial {
 
-    private final String value;
+	private final String value;
 
-    public Serial(String value) {
-        this.value = value;
-    }
+	public Serial(String value) {
+		this.value = value;
+	}
 
-    public boolean isEmpty() {
-        return value.isEmpty();
-    }
+	public boolean isEmpty() {
+		return value.isEmpty();
+	}
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(value);
-    }
+	@Override
+	public int hashCode() {
+		return Objects.hash(value);
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
+	@Override
+	public boolean equals(Object obj) {
 
-        if (!(obj instanceof Serial)) {
-            return false;
-        }
+		if ((obj instanceof Serial)) {
+			Serial other = (Serial) obj;
+			return new EqualsBuilder().append(value, other.value).isEquals();
+		} else {
+			return false;
+		}
 
-        Serial other = (Serial) obj;
-
-        return Objects.equals(value, other.value);
-    }
+	}
 
 }
