c46af2b4f6f02380f1551bad9beebb0c5538cd08
Dario Silva <dario@linux-3g8s.site>
2014-03-01 15:43:11 -0500

    InstrumentDTO

---
+++ src/main/java/ca/ulaval/ift6002/m2/infrastructure/persistence/dto/InstrumentDTO.java
@@ -0,0 +1,26 @@
+package ca.ulaval.ift6002.m2.infrastructure.persistence.dto;
+
+import javax.persistence.Entity;
+
+@Entity
+public class InstrumentDTO extends BaseDTO {
+
+    public String status;
+
+    public int serial;
+
+    public String typecode;
+
+    public InstrumentDTO(String status, String typecode, int serial) {
+        this.status = status;
+        this.serial = serial;
+        this.typecode = typecode;
+    }
+
+    protected InstrumentDTO() {
+        status = "";
+        serial = 0;
+        typecode = "";
+
+    }
+}
