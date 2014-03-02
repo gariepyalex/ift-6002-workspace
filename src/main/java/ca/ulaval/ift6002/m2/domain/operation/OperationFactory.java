5f7d92f170dacc9960398dfda2ae05dee12ddb3c
ALWAL12 <alexandre.walsh.1@ulaval.ca>
2014-03-02 14:40:00 -0500

    Minor refactor in OperationFactory

--- src/main/java/ca/ulaval/ift6002/m2/domain/operation/OperationFactory.java
+++ src/main/java/ca/ulaval/ift6002/m2/domain/operation/OperationFactory.java
@@ -1,6 +1,5 @@
 package ca.ulaval.ift6002.m2.domain.operation;
 
-import java.security.InvalidParameterException;
 import java.util.Date;
 
 import ca.ulaval.ift6002.m2.domain.operation.dangerous.EyeOperation;
@@ -16,22 +15,18 @@ public class OperationFactory {
 
     public Operation create(OperationType type, String description, Surgeon surgeon, Date date, Room room,
             OperationStatus status, Patient patient) {
-        Operation operation;
-
         if (type == OperationType.EYE) {
-            operation = new EyeOperation(description, surgeon, date, room, status, patient);
+            return new EyeOperation(description, surgeon, date, room, status, patient);
         } else if (type == OperationType.HEART) {
-            operation = new HeartOperation(description, surgeon, date, room, status, patient);
+            return new HeartOperation(description, surgeon, date, room, status, patient);
         } else if (type == OperationType.MARROW) {
-            operation = new MarrowOperation(description, surgeon, date, room, status, patient);
+            return new MarrowOperation(description, surgeon, date, room, status, patient);
         } else if (type == OperationType.ONCOLOGY) {
-            operation = new OncologicalOperation(description, surgeon, date, room, status, patient);
+            return new OncologicalOperation(description, surgeon, date, room, status, patient);
         } else if (type == OperationType.OTHER) {
-            operation = new RegularOperation(description, surgeon, date, room, status, patient);
-        } else {
-            throw new InvalidParameterException("operation type is not defined");
+            return new RegularOperation(description, surgeon, date, room, status, patient);
         }
 
-        return operation;
+        throw new IllegalArgumentException("Operation type is not defined");
     }
 }
