eb3d4c573d2c5d333c61e909cfd8ff5f2c5c7ac7
Mathieu <mathieu_carpentier@hotmail.com>
2014-03-02 09:57:45 -0500

    Added Value Object Description for real

--- src/main/java/ca/ulaval/ift6002/m2/application/responses/OperationResponse.java
+++ src/main/java/ca/ulaval/ift6002/m2/application/responses/OperationResponse.java
@@ -2,28 +2,35 @@ package ca.ulaval.ift6002.m2.application.responses;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
-
-import ca.ulaval.ift6002.m2.domain.operation.OperationStatus;
-import ca.ulaval.ift6002.m2.domain.operation.OperationType;
 
 @XmlRootElement(name = "operation")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class OperationResponse {
     public String description;
+
+    @XmlElement(name = "chirurgien")
     public Integer surgeon;
+
     public String date;
+
+    @XmlElement(name = "salle")
     public String room;
-    public OperationType type;
-    public OperationStatus status;
+    public String type;
+
+    @XmlElement(name = "statut")
+    public String status;
+
+    @XmlElement(name = "patient")
     public Integer patientNumber;
 
     protected OperationResponse() {
 
     }
 
-    public OperationResponse(String description, Integer surgeon, String date, String room, OperationType type,
-            OperationStatus status, Integer patientNumber) {
+    public OperationResponse(String description, Integer surgeon, String date, String room, String type, String status,
+            Integer patientNumber) {
         this.description = description;
         this.surgeon = surgeon;
         this.date = date;
