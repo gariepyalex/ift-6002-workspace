f85816b5e378bceda8ea23ce2afacd0cdd5d7ec1
Mathieu <mathieu_carpentier@hotmail.com>
2014-03-02 15:17:26 -0500

    Fix merge problem

--- src/main/java/ca/ulaval/ift6002/m2/application/assemblers/OperationResponseAssembler.java
+++ src/main/java/ca/ulaval/ift6002/m2/application/assemblers/OperationResponseAssembler.java
@@ -29,56 +29,20 @@ public class OperationResponseAssembler {
 
     public Operation fromResponse(OperationResponse response) throws InvalidResponseException {
 
-        Patient aPatient = patientRepository.get(response.patientNumber);
-        Date aDate = formatterDate.parse(response.date);
-        Surgeon aSurgeon = new Surgeon(response.surgeon);
-        Room aRoom = new Room(response.room);
-        String aDescription = response.description;
-        OperationType type = convertType(response.type);
-        OperationStatus status = convertStatus(response.status);
+        try {
 
-        return operationFactory.create(type, aDescription, aSurgeon, aDate, aRoom, status, aPatient);
+            Patient aPatient = patientRepository.get(response.patientNumber);
+            Date aDate = formatterDate.parse(response.date);
+            Surgeon aSurgeon = new Surgeon(response.surgeon);
+            Room aRoom = new Room(response.room);
+            String aDescription = response.description;
+            OperationType type = OperationType.determineFrom(response.type);
+            OperationStatus status = OperationStatus.determineFrom(response.status);
 
-    }
+            return operationFactory.create(type, aDescription, aSurgeon, aDate, aRoom, status, aPatient);
 
-    private OperationType convertType(String type) throws InvalidResponseException {
-        type = type.toUpperCase();
-        OperationType resultOperationType;
-        if (type.equals("OEIL")) {
-            resultOperationType = OperationType.EYE;
-        } else if (type.equals("COEUR")) {
-            resultOperationType = OperationType.HEART;
-        } else if (type.equals("MOELLE")) {
-            resultOperationType = OperationType.MARROW;
-        } else if (type.equals("ONCOLOGIQUE")) {
-            resultOperationType = OperationType.ONCOLOGY;
-        } else if (type.equals("AUTRE")) {
-            resultOperationType = OperationType.OTHER;
-        } else {
-            throw new InvalidResponseException("Operation type is not defined");
+        } catch (IllegalArgumentException e) {
+            throw new InvalidResponseException(e.getMessage());
         }
-        return resultOperationType;
-
-    }
-
-    private OperationStatus convertStatus(String status) throws InvalidResponseException {
-        status = status.toUpperCase();
-        OperationStatus resultOperationStatus;
-
-        if (status.equals("PLANIFIEE") || status.equals("")) {
-            resultOperationStatus = OperationStatus.PLANNED;
-        } else if (status.equals("EN_COURS")) {
-            resultOperationStatus = OperationStatus.IN_PROGRESS;
-        } else if (status.equals("TERMINE")) {
-            resultOperationStatus = OperationStatus.FINISH;
-        } else if (status.equals("ANNULEE")) {
-            resultOperationStatus = OperationStatus.CANCELLED;
-        } else if (status.equals("REPORTEE")) {
-            resultOperationStatus = OperationStatus.POSTPONED;
-        } else {
-            throw new InvalidResponseException("Operation status is not defined");
-        }
-
-        return resultOperationStatus;
     }
 }
