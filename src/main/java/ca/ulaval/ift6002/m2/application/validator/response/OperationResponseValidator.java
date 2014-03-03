4d3565b55a49aa7f59fe55ad9c7beed5cf923e9c
ALWAL12 <alexandre.walsh.1@ulaval.ca>
2014-03-03 01:34:43 -0500

    Added some cases in OperationValidator

--- src/main/java/ca/ulaval/ift6002/m2/application/validator/response/OperationResponseValidator.java
+++ src/main/java/ca/ulaval/ift6002/m2/application/validator/response/OperationResponseValidator.java
@@ -7,9 +7,17 @@ public class OperationResponseValidator implements ResponseValidator<OperationRe
     @Override
     public void validate(OperationResponse response) throws InvalidResponseException {
         if (response.description.isEmpty()) {
-            throw new InvalidResponseException("Description is empty!");
+            throw new InvalidResponseException("Description is empty");
+        } else if (response.surgeon == null) {
+            throw new InvalidResponseException("Surgeon number must not be null");
+        } else if (response.date.isEmpty()) {
+            throw new InvalidResponseException("Date is empty");
         } else if (response.room.isEmpty()) {
-            throw new InvalidResponseException("Room is empty!");
+            throw new InvalidResponseException("Room is empty");
+        } else if (response.type.isEmpty()) {
+            throw new InvalidResponseException("Type is empty");
+        } else if (response.patientNumber == null) {
+            throw new InvalidResponseException("Patient number must not be null");
         }
     }
 
