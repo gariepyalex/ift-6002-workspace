ff4e93205d8f84a3d5f3c6a0ab84dec1291d2d21
ALWAL12 <alexandre.walsh.1@ulaval.ca>
2014-03-03 03:43:23 -0500

    Renamed few variables

--- src/test/java/ca/ulaval/ift6002/m2/application/assemblers/OperationResponseAssemblerTest.java
+++ src/test/java/ca/ulaval/ift6002/m2/application/assemblers/OperationResponseAssemblerTest.java
@@ -30,35 +30,36 @@ public class OperationResponseAssemblerTest {
     private static final String INVALID_STATUS = "Vivant";
     private static final String INVALID_TYPE = "House";
     private static final int PATIENT_NUMBER = 3;
-    private static final String A_ROOM = "une room";
-    private static final String ASTRING_DATE = "2013-10-10T10:10:10";
+    private static final String ROOM = "une room";
+    private static final String DATE_AS_STRING = "2013-10-10T10:10:10";
     private static final int SURGEON_NUMBER = 2;
     private static final String RANDOM_DESCRIPTOR = "random descriptor";
     private static final String TYPE = "oeil";
     private static final String STATUS = "planifiee";
-    private static final Date ADATE = new Date(1220227200L * 1000);
+    private static final Date DATE = new Date(1220227200L * 1000);
 
     @Mock
-    OperationFactory operationFactory;
+    private OperationFactory operationFactory;
+
     @Mock
-    PatientRepository patientRepository;
+    private PatientRepository patientRepository;
+
     @Mock
-    DateFormatter formatterDate;
+    private DateFormatter formatterDate;
 
     @InjectMocks
     OperationResponseAssembler operationAssembler;
 
     private final OperationResponse operationResponse = new OperationResponse(RANDOM_DESCRIPTOR, SURGEON_NUMBER,
-            ASTRING_DATE, A_ROOM, TYPE, STATUS, PATIENT_NUMBER);
+            DATE_AS_STRING, ROOM, TYPE, STATUS, PATIENT_NUMBER);
 
     @Before
     public void setUp() {
-        willReturn(ADATE).given(formatterDate).parse(ASTRING_DATE);
+        willReturn(DATE).given(formatterDate).parse(DATE_AS_STRING);
     }
 
     @Test
     public void whenFromResponseCallPatientRepositoryGetShouldBeCall() throws InvalidResponseException {
-
         operationAssembler.fromResponse(operationResponse);
 
         verify(patientRepository).get(PATIENT_NUMBER);
@@ -66,35 +67,31 @@ public class OperationResponseAssemblerTest {
 
     @Test
     public void whenFromResponseCallFormatterDateParseShouldBeCall() throws InvalidResponseException {
-
         operationAssembler.fromResponse(operationResponse);
 
-        verify(formatterDate).parse(ASTRING_DATE);
+        verify(formatterDate).parse(DATE_AS_STRING);
 
     }
 
     @Test
     public void whenFromResponseCallFactoryCreateShouldBeCalled() throws InvalidResponseException {
-        OperationResponse response = new OperationResponse(RANDOM_DESCRIPTOR, SURGEON_NUMBER, ASTRING_DATE, A_ROOM,
+        OperationResponse response = new OperationResponse(RANDOM_DESCRIPTOR, SURGEON_NUMBER, DATE_AS_STRING, ROOM,
                 TYPE, STATUS, PATIENT_NUMBER);
-
         operationAssembler.fromResponse(response);
-
         verify(operationFactory).create(any(OperationType.class), any(String.class), any(Surgeon.class),
                 any(Date.class), any(Room.class), any(OperationStatus.class), any(Patient.class));
     }
 
     @Test(expected = InvalidResponseException.class)
     public void whenFromResponseCallWithInvalidTypeShouldThrowException() throws InvalidResponseException {
-        OperationResponse response = new OperationResponse(RANDOM_DESCRIPTOR, SURGEON_NUMBER, ASTRING_DATE, A_ROOM,
+        OperationResponse response = new OperationResponse(RANDOM_DESCRIPTOR, SURGEON_NUMBER, DATE_AS_STRING, ROOM,
                 INVALID_TYPE, STATUS, PATIENT_NUMBER);
-
         operationAssembler.fromResponse(response);
     }
 
     @Test(expected = InvalidResponseException.class)
     public void whenFromResponseCallWithInvalidStatusShouldThrowException() throws InvalidResponseException {
-        OperationResponse response = new OperationResponse(RANDOM_DESCRIPTOR, SURGEON_NUMBER, ASTRING_DATE, A_ROOM,
+        OperationResponse response = new OperationResponse(RANDOM_DESCRIPTOR, SURGEON_NUMBER, DATE_AS_STRING, ROOM,
                 TYPE, INVALID_STATUS, PATIENT_NUMBER);
 
         operationAssembler.fromResponse(response);
