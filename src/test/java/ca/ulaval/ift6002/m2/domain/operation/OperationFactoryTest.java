7fa532dcc851748274a11c38074d1968778a875c
Darkskymaster <bedardjs97@hotmail.com>
2014-03-03 10:42:07 -0500

    Removed a warning

--- src/test/java/ca/ulaval/ift6002/m2/domain/operation/OperationFactoryTest.java
+++ src/test/java/ca/ulaval/ift6002/m2/domain/operation/OperationFactoryTest.java
@@ -1,6 +1,6 @@
 package ca.ulaval.ift6002.m2.domain.operation;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import java.util.Date;
 
@@ -21,55 +21,55 @@ import ca.ulaval.ift6002.m2.domain.surgeon.Surgeon;
 @RunWith(MockitoJUnitRunner.class)
 public class OperationFactoryTest {
 
-    private static final String DESCRIPTION = "description";
-    private static final OperationStatus STATUS = OperationStatus.PLANNED;
+	private static final String DESCRIPTION = "description";
+	private static final OperationStatus STATUS = OperationStatus.PLANNED;
 
-    private OperationFactory factory;
+	private OperationFactory factory;
 
-    @Mock
-    private Surgeon surgeon;
+	@Mock
+	private Surgeon surgeon;
 
-    @Mock
-    private Date date;
+	@Mock
+	private Date date;
 
-    @Mock
-    private Room room;
+	@Mock
+	private Room room;
 
-    @Mock
-    private Patient patient;
+	@Mock
+	private Patient patient;
 
-    @Before
-    public void setUp() {
-        factory = new OperationFactory();
-    }
+	@Before
+	public void setUp() {
+		factory = new OperationFactory();
+	}
 
-    @Test
-    public void whenTypeIsEyeShouldReturnEyeOperation() {
-        Operation operation = factory.create(OperationType.EYE, DESCRIPTION, surgeon, date, room, STATUS, patient);
-        assertTrue(operation instanceof EyeOperation);
-    }
+	@Test
+	public void whenTypeIsEyeShouldReturnEyeOperation() {
+		Operation operation = factory.create(OperationType.EYE, DESCRIPTION, surgeon, date, room, STATUS, patient);
+		assertTrue(operation instanceof EyeOperation);
+	}
 
-    @Test
-    public void whenTypeIsHeartShouldReturnHeartOperation() {
-        Operation operation = factory.create(OperationType.HEART, DESCRIPTION, surgeon, date, room, STATUS, patient);
-        assertTrue(operation instanceof HeartOperation);
-    }
+	@Test
+	public void whenTypeIsHeartShouldReturnHeartOperation() {
+		Operation operation = factory.create(OperationType.HEART, DESCRIPTION, surgeon, date, room, STATUS, patient);
+		assertTrue(operation instanceof HeartOperation);
+	}
 
-    @Test
-    public void whenTypeIsMarrowShouldReturnMarrowOperation() {
-        Operation operation = factory.create(OperationType.MARROW, DESCRIPTION, surgeon, date, room, STATUS, patient);
-        assertTrue(operation instanceof MarrowOperation);
-    }
+	@Test
+	public void whenTypeIsMarrowShouldReturnMarrowOperation() {
+		Operation operation = factory.create(OperationType.MARROW, DESCRIPTION, surgeon, date, room, STATUS, patient);
+		assertTrue(operation instanceof MarrowOperation);
+	}
 
-    @Test
-    public void whenTypeIsOncolyShouldReturnOncologicalOperation() {
-        Operation operation = factory.create(OperationType.ONCOLOGY, DESCRIPTION, surgeon, date, room, STATUS, patient);
-        assertTrue(operation instanceof OncologicalOperation);
-    }
+	@Test
+	public void whenTypeIsOncolyShouldReturnOncologicalOperation() {
+		Operation operation = factory.create(OperationType.ONCOLOGY, DESCRIPTION, surgeon, date, room, STATUS, patient);
+		assertTrue(operation instanceof OncologicalOperation);
+	}
 
-    @Test(expected = IllegalArgumentException.class)
-    public void whenCreateWithUnknownTypeShouldThrowException() {
-        Operation operation = factory.create(OperationType.OTHER, DESCRIPTION, surgeon, date, room, STATUS, patient);
-    }
+	@Test(expected = IllegalArgumentException.class)
+	public void whenCreateWithUnknownTypeShouldThrowException() {
+		factory.create(OperationType.OTHER, DESCRIPTION, surgeon, date, room, STATUS, patient);
+	}
 
 }
