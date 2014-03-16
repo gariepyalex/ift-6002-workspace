829fd7c5bd82f64de3103923932b485a3a8ea2f9
moduv5 <marc-olivier.duval.1@ulaval.ca>
2014-03-16 15:46:38 -0400

    Created package Factory and add Skeleton for InstrumentFactory

---
+++ src/main/java/ca/ulaval/ift6002/m2/factory/hibernate/InstrumentHibernateFactory.java
@@ -0,0 +1,17 @@
+package ca.ulaval.ift6002.m2.factory.hibernate;
+
+import ca.ulaval.ift6002.m2.domain.instrument.Instrument;
+import ca.ulaval.ift6002.m2.domain.instrument.InstrumentFactory;
+import ca.ulaval.ift6002.m2.domain.instrument.InstrumentStatus;
+import ca.ulaval.ift6002.m2.domain.instrument.Serial;
+import ca.ulaval.ift6002.m2.domain.instrument.Typecode;
+
+public class InstrumentHibernateFactory implements InstrumentFactory {
+
+    @Override
+    public Instrument create(Typecode typecode, InstrumentStatus status, Serial serialNumber) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
