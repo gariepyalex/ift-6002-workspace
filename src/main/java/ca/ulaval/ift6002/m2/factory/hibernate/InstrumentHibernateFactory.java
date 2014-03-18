edc5238d8b6e36919a5c3c8053b263b600e450fb
moduv5 <marc-olivier.duval.1@ulaval.ca>
2014-03-18 11:39:23 -0400

    Implement method in InstrumentHibernateFactory

--- src/main/java/ca/ulaval/ift6002/m2/factory/hibernate/InstrumentHibernateFactory.java
+++ src/main/java/ca/ulaval/ift6002/m2/factory/hibernate/InstrumentHibernateFactory.java
@@ -5,19 +5,18 @@ import ca.ulaval.ift6002.m2.domain.instrument.InstrumentFactory;
 import ca.ulaval.ift6002.m2.domain.instrument.InstrumentStatus;
 import ca.ulaval.ift6002.m2.domain.instrument.Serial;
 import ca.ulaval.ift6002.m2.domain.instrument.Typecode;
+import ca.ulaval.ift6002.m2.infrastructure.persistence.hibernate.entities.InstrumentHibernate;
 
 public class InstrumentHibernateFactory implements InstrumentFactory {
 
     @Override
     public Instrument create(Typecode typecode, InstrumentStatus status, Serial serialNumber) {
-        // TODO Auto-generated method stub
-        return null;
+        return new InstrumentHibernate(typecode, status, serialNumber);
     }
 
     @Override
     public Instrument create(Typecode typecode, InstrumentStatus status) {
-        // TODO Auto-generated method stub
-        return null;
+        return create(typecode, status, new Serial(""));
     }
 
 }
