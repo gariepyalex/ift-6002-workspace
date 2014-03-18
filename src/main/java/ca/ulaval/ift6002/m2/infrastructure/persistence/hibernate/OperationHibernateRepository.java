01a7ae7739bece538a8f5cf80057f27eb7dbd489
moduv5 <marc-olivier.duval.1@ulaval.ca>
2014-03-18 18:21:21 -0400

    refactor of OperationHibernateRepository

--- src/main/java/ca/ulaval/ift6002/m2/infrastructure/persistence/hibernate/OperationHibernateRepository.java
+++ src/main/java/ca/ulaval/ift6002/m2/infrastructure/persistence/hibernate/OperationHibernateRepository.java
@@ -1,40 +1,46 @@
 package ca.ulaval.ift6002.m2.infrastructure.persistence.hibernate;
 
 import ca.ulaval.ift6002.m2.domain.operation.Operation;
+import ca.ulaval.ift6002.m2.domain.operation.OperationFactory;
 import ca.ulaval.ift6002.m2.domain.operation.OperationRepository;
+import ca.ulaval.ift6002.m2.factory.hibernate.OperationHibernateFactory;
 import ca.ulaval.ift6002.m2.infrastructure.persistence.assemblers.OperationDTOAssembler;
-import ca.ulaval.ift6002.m2.infrastructure.persistence.dto.OperationDTO;
+import ca.ulaval.ift6002.m2.infrastructure.persistence.hibernate.entities.OperationHibernateData;
 import ca.ulaval.ift6002.m2.infrastructure.persistence.provider.EntityManagerProvider;
 
-public class OperationHibernateRepository extends HibernateRepository<OperationDTO> implements OperationRepository {
+public class OperationHibernateRepository extends HibernateRepository<OperationHibernateData> implements
+        OperationRepository {
 
     private final OperationDTOAssembler operationAssembler;
+    private final OperationFactory operationFactory;
 
     public OperationHibernateRepository() {
-        super(OperationDTO.class);
+        super(OperationHibernateData.class);
         this.operationAssembler = new OperationDTOAssembler();
+        // TODO call factory locator here
+        this.operationFactory = new OperationHibernateFactory();
     }
 
     public OperationHibernateRepository(EntityManagerProvider entityManagerProvider,
-            OperationDTOAssembler operationAssembler) {
-        super(entityManagerProvider, OperationDTO.class);
+            OperationDTOAssembler operationAssembler, OperationFactory operationFactory) {
+        super(entityManagerProvider, OperationHibernateData.class);
         this.operationAssembler = operationAssembler;
+        this.operationFactory = operationFactory;
     }
 
     @Override
     public Operation get(int number) {
-        OperationDTO operationDTO = find(number);
+        OperationHibernateData operationData = find(number);
 
-        return operationAssembler.fromDTO(operationDTO);
+        return operationFactory.create(operationData.getType(), operationData);
     }
 
     @Override
     public void store(Operation operation) {
-        OperationDTO operationDTO = operationAssembler.toDTO(operation);
 
-        operationDTO = merge(operationDTO);
+        OperationHibernateData operationData = (OperationHibernateData) operation.getData();
 
-        operation.updateNumber(operationDTO.number);
+        merge(operationData);
     }
 
 }
