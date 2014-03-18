5e446c87c8bbc13e87e2a38d322319d5182b6596
moduv5 <marc-olivier.duval.1@ulaval.ca>
2014-03-18 16:56:50 -0400

    Create the OperationHibernateFactoryTest

---
+++ src/test/java/ca/ulaval/ift6002/m2/factory/hibernate/OperationHibernateFactoryTest.java
@@ -0,0 +1,41 @@
+package ca.ulaval.ift6002.m2.factory.hibernate;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Date;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import ca.ulaval.ift6002.m2.domain.operation.Operation;
+import ca.ulaval.ift6002.m2.domain.operation.OperationStatus;
+import ca.ulaval.ift6002.m2.domain.operation.OperationType;
+import ca.ulaval.ift6002.m2.domain.operation.room.Room;
+import ca.ulaval.ift6002.m2.domain.operation.surgeon.Surgeon;
+import ca.ulaval.ift6002.m2.domain.patient.Patient;
+import ca.ulaval.ift6002.m2.infrastructure.persistence.hibernate.entities.OperationHibernateData;
+
+public class OperationHibernateFactoryTest {
+
+    private static final OperationType TYPE = OperationType.EYE;
+    private static final OperationStatus STATUS = OperationStatus.PLANNED;
+    private static final String DESCRIPTION = "description";
+    private static final Surgeon SURGEON = new Surgeon(12345);
+    private static final Room ROOM = new Room("room");
+    private static final Patient PATIENT = new Patient(12345);
+    private static final Date DATE = new Date();
+
+    private OperationHibernateFactory operationHibernateFactory;
+
+    @Before
+    public void setUp() {
+        operationHibernateFactory = new OperationHibernateFactory();
+    }
+
+    @Test
+    public void whenCallingCreateShouldReturnOperationWithOperationHibernateData() {
+        Operation operation = operationHibernateFactory.create(TYPE, DESCRIPTION, SURGEON, DATE, ROOM, STATUS, PATIENT);
+
+        assertEquals(OperationHibernateData.class, operation.getData().getClass());
+    }
+}
