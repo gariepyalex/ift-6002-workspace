2c9560d71d4f92d3e2873a4c2cf4a265a8fdca30
moduv5 <marc-olivier.duval.1@ulaval.ca>
2014-03-19 16:15:36 -0400

    Correct some error and annotation in DrugHibernate

--- src/main/java/ca/ulaval/ift6002/m2/infrastructure/persistence/hibernate/entities/DrugHibernate.java
+++ src/main/java/ca/ulaval/ift6002/m2/infrastructure/persistence/hibernate/entities/DrugHibernate.java
@@ -1,28 +1,30 @@
 package ca.ulaval.ift6002.m2.infrastructure.persistence.hibernate.entities;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 import ca.ulaval.ift6002.m2.domain.drug.Din;
 import ca.ulaval.ift6002.m2.domain.drug.Drug;
 
+@Entity
 public class DrugHibernate extends Drug {
 
     @Id
     @GeneratedValue
     private Integer id;
-    private Din din;
+    private String din;
     private String brandName;
     private String descriptor;
 
     public DrugHibernate(Din din, String brandName) {
-        this.din = din;
+        this.din = din.getValue();
         this.brandName = brandName;
         this.descriptor = "";
     }
 
     public DrugHibernate(Din din, String brandName, String descriptor) {
-        this.din = din;
+        this.din = din.getValue();
         this.brandName = brandName;
         this.descriptor = descriptor;
     }
@@ -33,7 +35,7 @@ public class DrugHibernate extends Drug {
 
     @Override
     public Din getDin() {
-        return din;
+        return new Din(din);
     }
 
     @Override
