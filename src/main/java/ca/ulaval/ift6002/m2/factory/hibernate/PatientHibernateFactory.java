f454381713ac7ca3c005b9ae0eef5484f223d52d
ALWAL12 <alexandre.walsh.1@ulaval.ca>
2014-03-20 16:42:39 -0400

    Patient is now abstract

--- src/main/java/ca/ulaval/ift6002/m2/factory/hibernate/PatientHibernateFactory.java
+++ src/main/java/ca/ulaval/ift6002/m2/factory/hibernate/PatientHibernateFactory.java
@@ -5,19 +5,18 @@ import java.util.Collection;
 import ca.ulaval.ift6002.m2.domain.patient.Patient;
 import ca.ulaval.ift6002.m2.domain.patient.PatientFactory;
 import ca.ulaval.ift6002.m2.domain.prescription.Prescription;
+import ca.ulaval.ift6002.m2.infrastructure.persistence.hibernate.entities.PatientHibernate;
 
 public class PatientHibernateFactory implements PatientFactory {
 
     @Override
     public Patient create(int number, String healthInsuranceNumber) {
-        // TODO
-        return null;
+        return new PatientHibernate(number, healthInsuranceNumber);
     }
 
     @Override
     public Patient create(int number, String healthInsuranceNumber, Collection<Prescription> prescriptions) {
-        // TODO
-        return null;
+        return new PatientHibernate(number, healthInsuranceNumber, prescriptions);
     }
 
 }
--- src/main/java/ca/ulaval/ift6002/m2/infrastructure/persistence/hibernate/entities/PatientHibernate.java
+++ src/main/java/ca/ulaval/ift6002/m2/infrastructure/persistence/hibernate/entities/PatientHibernate.java
@@ -8,18 +8,21 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import ca.ulaval.ift6002.m2.domain.patient.Patient;
 import ca.ulaval.ift6002.m2.domain.prescription.Prescription;
+import ca.ulaval.ift6002.m2.domain.prescription.PrescriptionNotFoundException;
 
 @Entity
-public class PatientHibernate {
+public class PatientHibernate extends Patient {
 
     @Id
     public Integer number;
 
     @OneToMany(cascade = { CascadeType.ALL })
     // TODO use prescription hibernate
-    // public Collection<PrescriptionDTO> prescriptions;
+    public Collection<Prescription> prescriptions;
     public String healthInsuranceNumber;
+    public Boolean isDead;
 
     public PatientHibernate(Integer number, String healthInsuranceNumber) {
         this(number, healthInsuranceNumber, new ArrayList<Prescription>());
@@ -27,13 +30,56 @@ public class PatientHibernate {
 
     public PatientHibernate(Integer number, String healthInsuranceNumber, Collection<Prescription> prescriptions) {
         this.number = number;
-        // this.prescriptions = prescriptions;
+        this.prescriptions = prescriptions;
         this.healthInsuranceNumber = healthInsuranceNumber;
+        this.isDead = false;
+    }
+
+    @Override
+    protected void addPrescription(Prescription prescription) {
+        // TODO cast in PrescriptionHibernate
+        prescriptions.add(prescription);
+    }
+
+    @Override
+    public int countPrescriptions() {
+        return prescriptions.size();
+    }
+
+    @Override
+    public void declareDead() {
+        isDead = true;
+    }
+
+    @Override
+    public Collection<Prescription> getPrescriptions() {
+        // TODO Convert into domain prescription
+        return prescriptions;
+    }
+
+    @Override
+    public String getHealthInsuranceNumber() {
+        return healthInsuranceNumber;
+    }
+
+    @Override
+    public boolean isDead() {
+        return isDead;
+    }
+
+    @Override
+    protected Prescription findPrescription(int prescriptionNumber) {
+        // TODO do something with HibernatePrescription
+        for (Prescription prescription : prescriptions) {
+            if (prescription.hasNumber(prescriptionNumber)) {
+                return prescription;
+            }
+        }
+
+        throw new PrescriptionNotFoundException("No prescription found for number: " + prescriptionNumber);
     }
 
     protected PatientHibernate() {
-        this.number = 0;
-        // this.prescriptions = new ArrayList<PrescriptionDTO>();
-        this.healthInsuranceNumber = "";
+        this(0, "", new ArrayList<Prescription>());
     }
 }
