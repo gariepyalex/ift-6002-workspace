744b7de4e45baa0cf251b0da293e922ec32c4939
ALWAL12 <alexandre.walsh.1@ulaval.ca>
2014-03-20 16:51:24 -0400

    Updated PatientHibernateRepository

--- src/main/java/ca/ulaval/ift6002/m2/infrastructure/persistence/hibernate/PatientHibernateRepository.java
+++ src/main/java/ca/ulaval/ift6002/m2/infrastructure/persistence/hibernate/PatientHibernateRepository.java
@@ -2,36 +2,28 @@ package ca.ulaval.ift6002.m2.infrastructure.persistence.hibernate;
 
 import ca.ulaval.ift6002.m2.domain.patient.Patient;
 import ca.ulaval.ift6002.m2.domain.patient.PatientRepository;
-import ca.ulaval.ift6002.m2.infrastructure.persistence.assemblers.PatientDTOAssembler;
-import ca.ulaval.ift6002.m2.infrastructure.persistence.dto.PatientDTO;
+import ca.ulaval.ift6002.m2.infrastructure.persistence.hibernate.entities.PatientHibernate;
 import ca.ulaval.ift6002.m2.infrastructure.persistence.provider.EntityManagerProvider;
 
-public class PatientHibernateRepository extends HibernateRepository<PatientDTO> implements PatientRepository {
-
-    private final PatientDTOAssembler patientAssembler;
+public class PatientHibernateRepository extends HibernateRepository<PatientHibernate> implements PatientRepository {
 
     public PatientHibernateRepository() {
-        super(PatientDTO.class);
-        this.patientAssembler = new PatientDTOAssembler();
-    }
-
-    public PatientHibernateRepository(EntityManagerProvider entityManagerProvider, PatientDTOAssembler patientAssembler) {
-        super(entityManagerProvider, PatientDTO.class);
-        this.patientAssembler = patientAssembler;
+        super(PatientHibernate.class);
     }
 
     @Override
     public Patient get(int number) {
-        PatientDTO dto = find(number);
-
-        return patientAssembler.fromDTO(dto);
+        return find(number);
     }
 
     @Override
     public void store(Patient patient) {
-        PatientDTO patientDTO = patientAssembler.toDTO(patient);
+        PatientHibernate patientHibernate = (PatientHibernate) patient;
 
-        merge(patientDTO);
+        merge(patientHibernate);
     }
 
+    protected PatientHibernateRepository(EntityManagerProvider entityManagerProvider) {
+        super(entityManagerProvider, PatientHibernate.class);
+    }
 }
