9eb239a0f6803c4e836052e81f597189a4c02347
moduv5 <marc-olivier.duval.1@ulaval.ca>
2014-03-22 16:23:21 -0400

    Correct error in PrescriptionTest

--- src/test/java/ca/ulaval/ift6002/m2/domain/prescription/PrescriptionTest.java
+++ src/test/java/ca/ulaval/ift6002/m2/domain/prescription/PrescriptionTest.java
@@ -1,103 +1,123 @@
 package ca.ulaval.ift6002.m2.domain.prescription;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.BDDMockito.willReturn;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.CALLS_REAL_METHODS;
+import static org.mockito.Mockito.mock;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Date;
+
+import org.junit.Test;
 
 public class PrescriptionTest {
 
-    // private static final String PRACTIONNER_NAME = "practionner name";
-    // private static final Date DATE = new Date();
-    // private static final int FIVE_RENEWALS = 5;
-    // private static final int ZERO_RENEWALS = 0;
-    //
-    // private static final Pharmacy PHARMACY = new
-    // Pharmacy("pharmacy description");
-    //
-    // private static final Consumption CONSUMPTION_WITH_COUNT_ONE = new
-    // Consumption(DATE, PHARMACY, 1);
-    // private static final Consumption CONSUMPTION_WITH_COUNT_TWO = new
-    // Consumption(DATE, PHARMACY, 2);
-    //
-    // private static final Drug DRUG = mock(Drug.class);
-    //
-    // private Prescription prescription;
-    //
-    // @Test(expected = NotEnoughRenewalsException.class)
-    // public void givenZeroRenewalsWhenAddConsumptionShouldThrowException() {
-    // setupPrescriptionWithNoRenewals();
-    // prescription.addConsumption(CONSUMPTION_WITH_COUNT_ONE);
-    // }
-    //
-    // @Test
-    // public void
-    // givenFiveRenewalsWhenHavingConsumptionWithCountOneShouldHaveFourRemainingRenewals()
-    // {
-    // setupPrescriptionWithFiveRenewals();
-    // willReturn(Arrays.asList(CONSUMPTION_WITH_COUNT_ONE)).given(prescription).getConsumptions();
-    //
-    // assertEquals(4, prescription.remainingRenewals());
-    // }
-    //
-    // @Test
-    // public void
-    // givenFiveRenewalsWhenHavingConsumptionWithCountTwoShouldHaveThreeRemainingRenewals()
-    // {
-    // setupPrescriptionWithFiveRenewals();
-    // willReturn(Arrays.asList(CONSUMPTION_WITH_COUNT_TWO)).given(prescription).getConsumptions();
-    //
-    // assertEquals(3, prescription.remainingRenewals());
-    // }
-    //
-    // @Test
-    // public void givenPrescriptionWithNoRemainingRenewalsShouldBeObsolete() {
-    // setupPrescriptionWithNoRenewals();
-    // boolean isObsolete = prescription.isObsolete();
-    // assertTrue(isObsolete);
-    // }
-    //
-    // @Test
-    // public void givenPrescriptionWithOneOldConsumptionShouldBeObsolete() {
-    // setupPrescriptionWithFiveRenewals();
-    // Consumption oldConsumption = consumptionOfSevenMonthsAgo();
-    // willReturn(oldConsumption).given(prescription).lastConsumption();
-    //
-    // boolean isObsolete = prescription.isObsolete();
-    //
-    // assertTrue(isObsolete);
-    // }
-    //
-    // @Test
-    // public void
-    // givenPrescriptionWithOneRecentConsumptionShouldNotBeObsolete() {
-    // setupPrescriptionWithFiveRenewals();
-    // Consumption recentConsumption = consumptionOfOneMonthAgo();
-    // willReturn(recentConsumption).given(prescription).lastConsumption();
-    //
-    // boolean isObsolete = prescription.isObsolete();
-    //
-    // assertFalse(isObsolete);
-    // }
-    //
-    // private void setupPrescriptionWithFiveRenewals() {
-    // prescription = mock(Prescription.class, CALLS_REAL_METHODS);
-    // willReturn(FIVE_RENEWALS).given(prescription).getRenewals();
-    // }
-    //
-    // private void setupPrescriptionWithNoRenewals() {
-    // prescription = mock(Prescription.class, CALLS_REAL_METHODS);
-    // willReturn(ZERO_RENEWALS).given(prescription).getRenewals();
-    // }
-    //
-    // private Consumption consumptionOfSevenMonthsAgo() {
-    // Date sevenMonthsAgo = mock(Date.class);
-    // willReturn(false).given(sevenMonthsAgo).after(any(Date.class));
-    //
-    // return new Consumption(sevenMonthsAgo, PHARMACY, 1);
-    // }
-    //
-    // private Consumption consumptionOfOneMonthAgo() {
-    // Date oneMonthsAgo = mock(Date.class);
-    // willReturn(true).given(oneMonthsAgo).after(any(Date.class));
-    //
-    // return new Consumption(oneMonthsAgo, PHARMACY, 1);
-    // }
+    private static final int FIVE_RENEWALS = 5;
+    private static final int ZERO_RENEWALS = 0;
+    private static final int FOUR_REMAINING_RENEWALS = 4;
+    private static final int THREE_REMAINING_RENEWALS = 3;
 
+    private static final Consumption CONSUMPTION = mock(Consumption.class);
+
+    private static final Collection<Consumption> CONSUMPTIONS = Arrays.asList(CONSUMPTION);
+
+    private Prescription prescription;
+
+    @Test(expected = NotEnoughRenewalsException.class)
+    public void givenZeroRenewalsWhenAddConsumptionShouldThrowException() {
+        setUpConsumptionWithCountOne();
+        setupPrescriptionWithNoRenewals();
+        prescription.addConsumption(CONSUMPTION);
+    }
+
+    @Test
+    public void givenFiveRenewalsWhenHavingConsumptionWithCountOneShouldHaveFourRemainingRenewals() {
+        setUpConsumptionWithCountOne();
+        setupPrescriptionWithFiveRenewals();
+        willReturn(Arrays.asList(CONSUMPTION)).given(prescription).getConsumptions();
+
+        assertEquals(FOUR_REMAINING_RENEWALS, prescription.remainingRenewals());
+    }
+
+    @Test
+    public void givenFiveRenewalsWhenHavingConsumptionWithCountTwoShouldHaveThreeRemainingRenewals() {
+        setUpConsumptionWithCountTwo();
+        setupPrescriptionWithFiveRenewals();
+        willReturn(Arrays.asList(CONSUMPTION)).given(prescription).getConsumptions();
+
+        assertEquals(THREE_REMAINING_RENEWALS, prescription.remainingRenewals());
+    }
+
+    @Test
+    public void givenPrescriptionWithNoRemainingRenewalsShouldBeObsolete() {
+        setupPrescriptionWithNoRenewals();
+        boolean isObsolete = prescription.isObsolete();
+        assertTrue(isObsolete);
+    }
+
+    @Test
+    public void givenPrescriptionWithOneOldConsumptionShouldBeObsolete() {
+        setupPrescriptionWithFiveRenewals();
+        setUpConsumptionOfSevenMonthsAgo();
+        willReturn(CONSUMPTION).given(prescription).lastConsumption();
+
+        boolean isObsolete = prescription.isObsolete();
+
+        assertTrue(isObsolete);
+    }
+
+    @Test
+    public void givenPrescriptionWithOneRecentConsumptionShouldNotBeObsolete() {
+        setupPrescriptionWithFiveRenewals();
+        setUpConsumptionOfOneMonthAgo();
+        willReturn(CONSUMPTION).given(prescription).lastConsumption();
+
+        boolean isObsolete = prescription.isObsolete();
+
+        assertFalse(isObsolete);
+    }
+
+    private void setupPrescription() {
+        willReturn(CONSUMPTIONS).given(prescription).getConsumptions();
+        willReturn(CONSUMPTION).given(prescription).lastConsumption();
+        willReturn(false).given(prescription).isConsumptionsEmpty();
+    }
+
+    private void setupPrescriptionWithFiveRenewals() {
+        prescription = mock(Prescription.class, CALLS_REAL_METHODS);
+        setupPrescription();
+        willReturn(FIVE_RENEWALS).given(prescription).getRenewals();
+    }
+
+    private void setupPrescriptionWithNoRenewals() {
+        prescription = mock(Prescription.class, CALLS_REAL_METHODS);
+        setupPrescription();
+        willReturn(ZERO_RENEWALS).given(prescription).getRenewals();
+    }
+
+    private void setUpConsumptionOfSevenMonthsAgo() {
+        Date sevenMonthsAgo = mock(Date.class);
+        willReturn(false).given(sevenMonthsAgo).after(any(Date.class));
+
+        willReturn(sevenMonthsAgo).given(CONSUMPTION).getDate();
+    }
+
+    private void setUpConsumptionOfOneMonthAgo() {
+        Date oneMonthsAgo = mock(Date.class);
+        willReturn(true).given(oneMonthsAgo).after(any(Date.class));
+
+        willReturn(oneMonthsAgo).given(CONSUMPTION).getDate();
+    }
+
+    private void setUpConsumptionWithCountOne() {
+        willReturn(1).given(CONSUMPTION).getCount();
+    }
+
+    private void setUpConsumptionWithCountTwo() {
+        willReturn(2).given(CONSUMPTION).getCount();
+    }
 }
