95547aee72ba31740d0215e9b5bcc42d0c225ab8
ALWAL12 <alexandre.walsh.1@ulaval.ca>
2014-03-24 13:07:48 -0400

    Created the QueryBuilder

---
+++ src/main/java/ca/ulaval/ift6002/m2/infrastructure/persistence/QueryBuilder.java
@@ -0,0 +1,24 @@
+package ca.ulaval.ift6002.m2.infrastructure.persistence;
+
+import java.util.List;
+
+public abstract class QueryBuilder<E> {
+
+    public abstract QueryBuilder<E> query(String query);
+
+    public abstract QueryBuilder<E> parameter(String name, Object value);
+
+    public abstract int perform();
+
+    public abstract E get();
+
+    public abstract List<E> list();
+
+    public List<E> list(int limit) {
+        return list(limit);
+    }
+
+    public List<E> list(int startIndex, int limit) {
+        return list().subList(startIndex, limit);
+    }
+}
---
+++ src/main/java/ca/ulaval/ift6002/m2/infrastructure/persistence/hibernate/HibernateQueryBuilder.java
@@ -0,0 +1,75 @@
+package ca.ulaval.ift6002.m2.infrastructure.persistence.hibernate;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.NoSuchElementException;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.TypedQuery;
+
+import ca.ulaval.ift6002.m2.infrastructure.persistence.QueryBuilder;
+
+public class HibernateQueryBuilder<E> extends QueryBuilder<E> {
+
+    private final EntityManager entityManager;
+    private final Class<E> classType;
+    private String query = null;
+    private Map<String, Object> parameters = new HashMap<>();
+
+    public HibernateQueryBuilder(EntityManager entityManager, Class<E> classType) {
+        this.entityManager = entityManager;
+        this.classType = classType;
+    }
+
+    @Override
+    public QueryBuilder<E> query(String query) {
+        this.query = query;
+        return this;
+    }
+
+    @Override
+    public QueryBuilder<E> parameter(String name, Object value) {
+        parameters.put(name, value);
+        return this;
+    }
+
+    @Override
+    public int perform() {
+        TypedQuery<E> typedQuery = entityManager.createQuery(query, classType);
+        assignParametersTo(typedQuery);
+
+        return typedQuery.executeUpdate();
+    }
+
+    @Override
+    public E get() {
+        try {
+            TypedQuery<E> typedQuery = entityManager.createQuery(query, classType);
+            assignParametersTo(typedQuery);
+
+            return typedQuery.getSingleResult();
+        } catch (NoResultException e) {
+            throw new NoSuchElementException("There are no element found.");
+        }
+    }
+
+    @Override
+    public List<E> list() {
+        TypedQuery<E> typedQuery = entityManager.createQuery(query, classType);
+        assignParametersTo(typedQuery);
+
+        return typedQuery.getResultList();
+    }
+
+    private TypedQuery<E> assignParametersTo(TypedQuery<E> typedQuery) {
+        for (Entry<String, Object> parameter : parameters.entrySet()) {
+            typedQuery.setParameter(parameter.getKey(), parameter.getValue());
+        }
+
+        return typedQuery;
+    }
+
+}
