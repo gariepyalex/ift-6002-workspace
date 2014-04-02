18cad84ded127992c15050ed99a36c6a09122f70
Alexandre Gariépy <gariepy.alex@gmail.com>
2014-04-02 14:49:50 -0400

    Adding a test case to Din

--- src/test/java/ca/ulaval/ift6002/m2/domain/drug/DinTest.java
+++ src/test/java/ca/ulaval/ift6002/m2/domain/drug/DinTest.java
@@ -1,5 +1,6 @@
 package ca.ulaval.ift6002.m2.domain.drug;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -7,11 +8,19 @@ import org.junit.Test;
 public class DinTest {
 
     private static final String EMPTY_NAME = "";
+    private static final String NON_EMPTY_NAME = "123456";
 
     @Test
     public void givenEmptyNameWhenIsEmptyShouldReturnTrue() {
         Din din = new Din(EMPTY_NAME);
         boolean isEmpty = din.isEmpty();
         assertTrue(isEmpty);
+    }
+
+    @Test
+    public void givenNonEmptyNameWhenIsEmptyShouldReturnFalse() {
+        Din din = new Din(NON_EMPTY_NAME);
+        boolean isEmpty = din.isEmpty();
+        assertFalse(isEmpty);
     }
 }
