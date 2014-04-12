eae50e12893dd513199b0f2d66eddfc821083f9c
ALWAL12 <alexandre.walsh.1@ulaval.ca>
2014-04-12 14:09:24 -0400

    Renamed Configuration to Configurable

--- src/main/java/ca/ulaval/ift6002/m2/configuration/Configurable.java
+++ src/main/java/ca/ulaval/ift6002/m2/configuration/Configurable.java
@@ -1,6 +1,6 @@
 package ca.ulaval.ift6002.m2.configuration;
 
-public interface Configuration {
+public interface Configurable {
 
     void configure();
 }
--- src/main/java/ca/ulaval/ift6002/m2/configuration/persistence/HibernatePersistanceConfiguration.java
+++ src/main/java/ca/ulaval/ift6002/m2/configuration/persistence/HibernatePersistanceConfiguration.java
@@ -1,6 +1,6 @@
 package ca.ulaval.ift6002.m2.configuration.persistence;
 
-import ca.ulaval.ift6002.m2.configuration.Configuration;
+import ca.ulaval.ift6002.m2.configuration.Configurable;
 import ca.ulaval.ift6002.m2.domain.drug.DrugRepository;
 import ca.ulaval.ift6002.m2.domain.operation.OperationRepository;
 import ca.ulaval.ift6002.m2.domain.patient.PatientRepository;
@@ -9,7 +9,7 @@ import ca.ulaval.ift6002.m2.infrastructure.persistence.hibernate.repositories.Op
 import ca.ulaval.ift6002.m2.infrastructure.persistence.hibernate.repositories.PatientHibernateRepository;
 import ca.ulaval.ift6002.m2.locator.RepositoryLocator;
 
-public class HibernatePersistanceConfiguration implements Configuration {
+public class HibernatePersistanceConfiguration implements Configurable {
 
     @Override
     public void configure() {
