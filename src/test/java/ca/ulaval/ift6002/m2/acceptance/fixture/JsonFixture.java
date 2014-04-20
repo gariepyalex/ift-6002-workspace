542aecb8ab70b312abd8690a4bd4a0ec25af4397
moduv5 <marc-olivier.duval.1@ulaval.ca>
2014-04-19 22:23:56 -0400

    Create fixture for json

---
+++ src/test/java/ca/ulaval/ift6002/m2/acceptance/fixture/JsonFixture.java
@@ -0,0 +1,27 @@
+package ca.ulaval.ift6002.m2.acceptance.fixture;
+
+import ca.ulaval.ift6002.m2.acceptance.DumboTheElephantStories;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.module.jaxb.JaxbAnnotationModule;
+
+public class JsonFixture {
+    private ObjectMapper jsonMapper;
+
+    public void JsonFixture() {
+        jsonMapper = new ObjectMapper();
+        JaxbAnnotationModule module = new JaxbAnnotationModule();
+        jsonMapper.registerModule(module);
+    }
+
+    public String convertToJson(Object obj) {
+        String json = "";
+        try {
+            json = DumboTheElephantStories.OBJECT_MAPPER.writeValueAsString(obj);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return json;
+    }
+}
