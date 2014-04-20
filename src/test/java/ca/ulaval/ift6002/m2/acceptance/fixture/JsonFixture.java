e49af281da6ec114a4ba206db7313bb51ffba369
moduv5 <marc-olivier.duval.1@ulaval.ca>
2014-04-19 22:27:06 -0400

    Correct a stupid copy and paste error

--- src/test/java/ca/ulaval/ift6002/m2/acceptance/fixture/JsonFixture.java
+++ src/test/java/ca/ulaval/ift6002/m2/acceptance/fixture/JsonFixture.java
@@ -1,7 +1,5 @@
 package ca.ulaval.ift6002.m2.acceptance.fixture;
 
-import ca.ulaval.ift6002.m2.acceptance.DumboTheElephantStories;
-
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.module.jaxb.JaxbAnnotationModule;
@@ -18,7 +16,7 @@ public class JsonFixture {
     public String convertToJson(Object obj) {
         String json = "";
         try {
-            json = DumboTheElephantStories.OBJECT_MAPPER.writeValueAsString(obj);
+            json = jsonMapper.writeValueAsString(obj);
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
