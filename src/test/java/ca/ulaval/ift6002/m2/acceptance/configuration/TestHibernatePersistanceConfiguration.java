6ad73bf70a62c52aab8b52905e2d9f1bfd879ecf
ALWAL12 <alexandre.walsh.1@ulaval.ca>
2014-04-25 18:29:14 -0400

    Revert "Mock repository in acceptance test"
    This reverts commit 036b71a9f93567baea5c90aac62e8fe33a22358f.

--- src/test/java/ca/ulaval/ift6002/m2/acceptance/configuration/TestHibernatePersistanceConfiguration.java
+++ src/test/java/ca/ulaval/ift6002/m2/acceptance/configuration/TestHibernatePersistanceConfiguration.java
@@ -1,6 +1,5 @@
 package ca.ulaval.ift6002.m2.acceptance.configuration;
 
-import static org.mockito.Mockito.mock;
 import ca.ulaval.ift6002.m2.configuration.Configurable;
 import ca.ulaval.ift6002.m2.domain.drug.DrugRepository;
 import ca.ulaval.ift6002.m2.domain.operation.OperationRepository;
@@ -8,17 +7,19 @@ import ca.ulaval.ift6002.m2.domain.patient.PatientRepository;
 import ca.ulaval.ift6002.m2.infrastructure.persistence.hibernate.repositories.DrugHibernateRepository;
 import ca.ulaval.ift6002.m2.infrastructure.persistence.hibernate.repositories.OperationHibernateRepository;
 import ca.ulaval.ift6002.m2.infrastructure.persistence.hibernate.repositories.PatientHibernateRepository;
+import ca.ulaval.ift6002.m2.infrastructure.persistence.provider.EntityManagerProvider;
+import ca.ulaval.ift6002.m2.infrastructure.persistence.provider.EntityManagerProviderGlobal;
 import ca.ulaval.ift6002.m2.locator.RepositoryLocator;
 
-public class TestPersistanceConfiguration implements Configurable {
+public class TestHibernatePersistanceConfiguration implements Configurable {
 
     @Override
     public void configure() {
         RepositoryLocator repositoryLocator = new RepositoryLocator();
-
-        repositoryLocator.register(DrugRepository.class, mock(DrugHibernateRepository.class));
-        repositoryLocator.register(PatientRepository.class, mock(PatientHibernateRepository.class));
-        repositoryLocator.register(OperationRepository.class, mock(OperationHibernateRepository.class));
+        EntityManagerProvider entityManagerProvider = new EntityManagerProviderGlobal();
+        repositoryLocator.register(DrugRepository.class, new DrugHibernateRepository(entityManagerProvider));
+        repositoryLocator.register(PatientRepository.class, new PatientHibernateRepository(entityManagerProvider));
+        repositoryLocator.register(OperationRepository.class, new OperationHibernateRepository(entityManagerProvider));
 
         RepositoryLocator.load(repositoryLocator);
 
--- src/test/java/ca/ulaval/ift6002/m2/acceptance/runners/JettyTestRunner.java
+++ src/test/java/ca/ulaval/ift6002/m2/acceptance/runners/JettyTestRunner.java
@@ -3,7 +3,7 @@ package ca.ulaval.ift6002.m2.acceptance.runners;
 import org.jbehave.core.annotations.BeforeStories;
 
 import ca.ulaval.ift6002.m2.JettyServer;
-import ca.ulaval.ift6002.m2.acceptance.configuration.TestPersistanceConfiguration;
+import ca.ulaval.ift6002.m2.acceptance.configuration.TestHibernatePersistanceConfiguration;
 import ca.ulaval.ift6002.m2.configuration.factory.HibernateFactoryConfiguration;
 
 public class JettyTestRunner {
@@ -13,8 +13,9 @@ public class JettyTestRunner {
 
     @BeforeStories
     public void startJetty() throws Exception {
+        // TODO CHANGE THIS TO MOCK
         new HibernateFactoryConfiguration().configure();
-        new TestPersistanceConfiguration().configure();
+        new TestHibernatePersistanceConfiguration().configure();
 
         server = new JettyServer(JETTY_TEST_PORT);
         server.start();
