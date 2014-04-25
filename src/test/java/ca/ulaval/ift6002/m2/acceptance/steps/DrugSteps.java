d6efc078268431a613560ef63924fae578df4701
ALWAL12 <alexandre.walsh.1@ulaval.ca>
2014-04-25 15:40:35 -0400

    Fixed a TODO! A useful Then steps! :)

--- src/test/java/ca/ulaval/ift6002/m2/acceptance/steps/DrugSteps.java
+++ src/test/java/ca/ulaval/ift6002/m2/acceptance/steps/DrugSteps.java
@@ -1,5 +1,8 @@
 package ca.ulaval.ift6002.m2.acceptance.steps;
 
+import static org.hamcrest.Matchers.hasItems;
+
+import org.jbehave.core.annotations.BeforeScenario;
 import org.jbehave.core.annotations.Then;
 import org.jbehave.core.annotations.When;
 import org.jbehave.core.steps.Steps;
@@ -15,7 +18,14 @@ public class DrugSteps extends Steps {
 
     private static final String EXISTING_KEYWORD = "Advil";
 
-    private static final String WILDCARD_KEYWORD = "Advil liqui";
+    private static final String WILDCARD_KEYWORD = "ANTI CLEAN";
+
+    private String[] expectedDins;
+
+    @BeforeScenario
+    public void clearResults() {
+        expectedDins = null;
+    }
 
     @When("je cherche un médicament avec moins de caractères que la limite requise")
     public void findDrugsWithLessCharatersThanLimitRequired() {
@@ -25,26 +35,36 @@ public class DrugSteps extends Steps {
     @When("je cherche des médicaments avec un mot-clé qui se retrouve dans quelques noms de médicaments")
     public void findDrugsWithNameKeyword() {
         findDrugs(EXISTING_KEYWORD);
+
+        expectDins("11111111", "22222222");
     }
 
     @When("je cherche des médicaments avec un mot-clé qui se retrouve dans quelques descriptions de médicaments")
     public void findDrugsWithDescriptionKeyword() {
         findDrugs(EXISTING_KEYWORD);
+
+        expectDins("11111111", "22222222");
     }
 
     @When("je cherche des médicaments avec un mot-clé qui contient un patron générique")
     public void findDrugsWithWildCard() {
         findDrugs(WILDCARD_KEYWORD);
+
+        expectDins("02239513", "02229501", "02229377");
     }
 
     @Then("la liste de médicaments retournée contient ceux-ci")
     public void drugsListCorrespondsToTheKeyword() {
-        // TODO assert something... non empty list? success?? ask teacher...
+        ResponseContext.getResponse().then().assertThat().body("drug.din", hasItems(expectedDins));
     }
 
     private void findDrugs(String keyword) {
         Response response = new RequestBuilder().doGet("/medicaments/dins/" + keyword);
         ResponseContext.setResponse(response);
+    }
+
+    private void expectDins(String... dins) {
+        expectedDins = dins;
     }
 
 }
