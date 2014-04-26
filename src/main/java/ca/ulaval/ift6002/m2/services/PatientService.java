63168d546165d2d494d5c4862e045ccbaf5ec777
moduv5 <marc-olivier.duval.1@ulaval.ca>
2014-04-26 00:38:06 -0400

    Remove a todo

--- src/main/java/ca/ulaval/ift6002/m2/services/PatientService.java
+++ src/main/java/ca/ulaval/ift6002/m2/services/PatientService.java
@@ -43,8 +43,6 @@ public class PatientService {
 
     public PrescriptionResponse[] getPrescriptions(String patientId) {
         Patient patient = getPatient(patientId);
-        // TODO SUMMARY SCenario is not finish !!! Prescription are not ordered
-        // by date !
         return prescriptionAssembler.toResponses(patient.getPrescriptions());
     }
 
