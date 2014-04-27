10edcf4bab5fd4327445de52c6c159569bf24f3c
moduv5 <marc-olivier.duval.1@ulaval.ca>
2014-04-27 11:48:40 -0400

    Correct a stupid bug with code coverage

--- src/test/java/ca/ulaval/ift6002/m2/infrastructure/persistence/hibernate/repositories/PatientHibernateRepositoryTest.java
+++ src/test/java/ca/ulaval/ift6002/m2/infrastructure/persistence/hibernate/repositories/PatientHibernateRepositoryTest.java
@@ -3,9 +3,9 @@ package ca.ulaval.ift6002.m2.infrastructure.persistence.hibernate.repositories;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.verify;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
@@ -22,8 +22,12 @@ public class PatientHibernateRepositoryTest {
     @Mock
     private HibernateRepository<PatientHibernate> hibernateRepository;
 
-    @InjectMocks
     private PatientHibernateRepository patientRepository;
+
+    @Before
+    public void setUp() {
+        patientRepository = new PatientHibernateRepository(hibernateRepository);
+    }
 
     @Test
     public void whenGettingPatientShouldVerifyHibernateRepository() {
